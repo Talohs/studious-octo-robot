bf0915060d7598608330c95aa2d0d0522efa06f9
Talohs <blackholesoul@gmail.com>
2023-01-25 01:48:24 -0500

    Readded _init_.py

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms import StringField, PasswordField, SubmitField, EmailField, TextAreaField
 from wtforms.validators import EqualTo, InputRequired
 
 class Signupform(FlaskForm):
@@ -9,4 +9,17 @@ class Signupform(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
     confirm = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField()
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField()
+
+class AddressForm(FlaskForm):
+    first_name = StringField('First name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
+    email = EmailField('Email', validators=[InputRequired()])
+    phone_number = StringField('Phone number', validators=[InputRequired()])
+    address = StringField('Address', validators=[InputRequired()])
     submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,12 @@
 from app import app
-from flask import render_template, redirect, url_for, flash
-from app.forms import Signupform
-from app.models import User
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required, current_user
+from app.forms import Signupform, LoginForm, PostForm
+from app.models import User, Address
 
 @app.route("/")
 def index():
     return render_template('index.html')
-
-@app.route('/posts')
-def posts():
-    return 'These are the posts!'
 
 @app.route("/sign_up", methods=['GET', 'POST'])
 def signup():
@@ -28,7 +25,48 @@ def signup():
         if check_user:
             flash('A user with that email and/or username already exists.', 'danger')
             return redirect(url_for('signup'))
-        
+
         new_user = User(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+
+        flash(f'Thank you {new_user.username} for using our app!', 'success')
+
         return redirect(url_for('index'))
+
     return render_template('sign-up.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Get the username and password from the form
+        username = form.username.data
+        password = form.password.data
+        print(username, password)
+        # Query the user table to see if there is a user with that username
+        user = User.query.filter_by(username=username).first()
+        # Check if there is a user and that the password is correct
+        if user is not None and user.check_password(password):
+            # log the user in
+            login_user(user)
+            flash(f"{user.username} is now logged in", "warning")
+            return redirect(url_for('index'))
+        else:
+            flash("Incorrect username and/or password", "danger")
+            return redirect(url_for('login'))
+        
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("You have been logged out", "warning")
+    return redirect(url_for('index'))
+
+@app.route('/contacts/<int:contact_id>')
+def get_contact(post_id):
+    # post = Post.query.get_or_404(post_id)
+    post = Address.query.get(post_id)
+    if not post:
+        flash(f"A post with id {post_id} does not exist", "danger")
+        return redirect(url_for('index'))
+    return render_template('post.html', post=post)
